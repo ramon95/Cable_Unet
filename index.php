8f29df465fe1aed5d39135f08c63fb2033b41b47
Ramon <ramon.a.p.r.95@gmail.com>
2018-08-30 18:37:54 -0400

    Creacion de archivos base

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include('template/header.php') ?>
+<?php include('template/nav.php') ?>
+            <div class="content">
+              <div class="container-fluid">
+              </div>
+            </div>
+<?php include('template/footer.php') ?>    
